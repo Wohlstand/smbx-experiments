22c4f29518a3178263a633f98f3a8caad8759546
Wohlstand <admin@wohlnet.ru>
2025-09-24 00:18:23 +0300

    Bump version to 1.3.8

--- copy-to-host.bat
+++ copy-to-host.bat
@@ -1,5 +1,6 @@
 @echo off
 set remoteroot=Z:\vitaly\.wine\drive_c\smbx1.3
+@echo on
 copy smbx-r.exe %remoteroot%
 copy MSWINSCK.ocx %remoteroot%
 copy SDL2MixerVB.dll %remoteroot%
@@ -11,4 +12,5 @@ copy cppticks.dll %remoteroot%
 copy "worlds\the invasion 2\save1.sav" "%remoteroot%\worlds\the invasion 2\save2.sav"
 copy screenshots\*.png "%remoteroot%\screenshots-xp"
 copy "%remoteroot%\worlds\the invasion 2\save1.sav" "worlds\the invasion 2\save2.sav"
+@echo off
 pause
